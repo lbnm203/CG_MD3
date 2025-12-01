a75cd43d6d85f1c02d40efac3b93ebc5b39ed217
lbnm203 <lebanhatminh1303@gmail.com>
2025-12-01 08:13:45 +0700

    update primary key

--- ss1_sql_base/student_management.sql
+++ ss1_sql_base/student_management.sql
@@ -1,4 +1,5 @@
 CREATE DATABASE IF NOT EXISTS student_management;
+DROP DATABASE IF EXISTS student_management;
 
 USE student_management;
 
@@ -7,21 +8,23 @@ CREATE TABLE student (
 	student_id INT,
     student_name VARCHAR(200),
     age INT,
-    student_country VARCHAR(50)
+    student_country VARCHAR(50),
+    PRIMARY KEY (student_id)
 );
 
 CREATE TABLE class (
 	class_id INT,
-    class_name VARCHAR(200)
+    class_name VARCHAR(200),
+    PRIMARY KEY (class_id)
 );
 
 CREATE TABLE teacher (
 	teacher_id INT,
     teacher_name VARCHAR(200),
     age INT,
-    teacher_country VARCHAR(200)
+    teacher_country VARCHAR(200),
+    PRIMARY KEY (teacher_id)
 );
-
 
 
 

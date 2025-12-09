74a1c69e58542104e9f1833141ac0a8200a1c69c
lbnm203 <lebanhatminh1303@gmail.com>
2025-12-09 22:38:33 +0700

    add responsive ss7-layout / add base-line bootstrap

---
+++ ss9_jsp_servlet/th_jsp_servlet/src/main/java/com/example/th_jsp_servlet/HelloServlet.java
@@ -0,0 +1,22 @@
+package com.example.th_jsp_servlet;
+
+import java.io.*;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        request.getRequestDispatcher("/WEB-INF/helloworld.jsp").forward(request, response);
+    }
+
+    public void destroy() {
+    }
+}
